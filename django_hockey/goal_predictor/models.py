d4b8807446ea39c580433fd86d2d246832fa4f3a
Brien Bennett <brienbennett@hotmail.com>
2023-04-27 16:56:28 -0400

    split Players model from Stats, split Stats into multiple stat classes to produce a table for each. defined attributes for PlayerGameLog based on the data extraction and population functions.

--- django_hockey/goal_predictor/models.py
+++ django_hockey/goal_predictor/models.py
@@ -34,42 +34,123 @@ class Team(models.Model):
     def __str__(self):
         return self.name
 
-    # def get_roster(self):
-    #     roster_data = http_get_roster(self.id)
-    #     self.roster = parse_roster_data(roster_data)
-    #     self.save()
 
 
 ## Create class Player where id, name are static and rest dynamic
 class Player(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=200)
-    info = models.JSONField(default=dict)
-    
+    nhl_id = models.IntegerField(default=0)
+    name = models.CharField(max_length=200)    
     position = models.CharField(max_length=10)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
 
-    game_log = models.JSONField(default=dict)
-    season_stats = models.JSONField(default=dict)
-    day_stats = models.JSONField(default=dict)
-    week_stats = models.JSONField(default=dict)
-    mon_stats = models.JSONField(default=dict)
-    div_stats = models.JSONField(default=dict)
-    league_stats = models.JSONField(default=dict)
-    home_away_stats = models.JSONField(default=dict)
-    w_l_stats = models.JSONField(default=dict)
-    sit_goals = models.JSONField(default=dict)
-    season_pace = models.JSONField(default=dict)
+    info = models.JSONField(default=dict)
 
-    schedule = models.JSONField(default=dict)
-
-    season = models.CharField(max_length=10, default=get_current_season)
+    season = models.CharField(max_length=10, default=get_current_season())
 
     def __str__(self):
         return self.name
 
-    # def get_stats(self):
-    #     stats_data = http_get_player(self.id)
-    #     self.stats = parse_player_stats(stats_data)
-    #     self.save()
+##Create classes for api stat calls, so that each produces an associated table to Player with foreign key
+
+class PlayerGameLog(models.Model):
+    # id = models.IntegerField(primary_key=True)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+
+    season = models.CharField(max_length=8)
+    time_on_ice = models.DurationField()
+    assists = models.IntegerField()
+    goals = models.IntegerField()
+    pim = models.IntegerField()
+    shots = models.IntegerField()
+    games = models.IntegerField()
+    hits = models.IntegerField()
+    power_play_goals = models.IntegerField()
+    power_play_points = models.IntegerField()
+    power_play_time_on_ice = models.DurationField()
+    even_time_on_ice = models.DurationField()
+    penalty_minutes = models.IntegerField()
+    faceoff_pct = models.FloatField()
+    shot_pct = models.FloatField()
+    game_winning_goals = models.IntegerField()
+    overtime_goals = models.IntegerField()
+    shorthanded_goals = models.IntegerField()
+    shorthanded_points = models.IntegerField()
+    shorthanded_time_on_ice = models.DurationField()
+    blocked = models.IntegerField()
+    plus_minus = models.IntegerField()
+    points = models.IntegerField()
+    shifts = models.IntegerField()
+    team_id = models.IntegerField()
+    team_name = models.CharField(max_length=50)
+    opponent_id = models.IntegerField()
+    opponent_name = models.CharField(max_length=50)
+    game_id = models.IntegerField()
+    game_link = models.URLField()
+    date = models.DateField()
+    is_home = models.BooleanField()
+    is_win = models.BooleanField()
+    is_ot = models.BooleanField()
+
+    # data = models.JSONField(default=dict)
+
+
+class PlayerSeasonStats(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    season = models.CharField(max_length=8)
+    data = models.JSONField(default=dict)
+
+
+class PlayerDivSplits(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    season = models.CharField(max_length=8)
+    data = models.JSONField(default=dict)
+
+
+class PlayerMonSplits(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    season = models.CharField(max_length=8)
+    data = models.JSONField(default=dict)
+
+
+class PlayerWeekSplits(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    season = models.CharField(max_length=8)
+    data = models.JSONField(default=dict)
+
+
+class PlayerLeagueStats(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    season = models.CharField(max_length=8)
+    data = models.JSONField(default=dict)
+
+
+class PlayerSeasonPace(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    season = models.CharField(max_length=8)
+    data = models.JSONField(default=dict)
+
+
+class PlayerSitGoals(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    season = models.CharField(max_length=8)
+    data = models.JSONField(default=dict)
+
+
+class PlayerDaySplits(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    season = models.CharField(max_length=8)
+    data = models.JSONField(default=dict)
+
+
+class PlayerHomeSplits(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    season = models.CharField(max_length=8)
+    data = models.JSONField(default=dict)
+
+
+class PlayerWinSplits(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    season = models.CharField(max_length=8)
+    data = models.JSONField(default=dict)
 
